00349005f39035a495e41e0df1d052d44627ca35
Fergal Moran <fergal.moran@gmail.com>
2020-09-28 13:35:39 +0100

    Build change

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
-docker run -v "$(pwd):/src/" \
-    --entrypoint /bin/sh \
-    cdrx/pyinstaller-linux \
-    -c "apt-get update -y && apt-get install -y portaudio19-dev && /entrypoint.sh"
+docker run \
+    -v "$(pwd):/src/" \
+    fergalmoran/disco-hue-builder:linux
